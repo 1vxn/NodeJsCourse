9248d3b2954980d33608405b3391b21070573901
1vxn <1vxn51@gmail.com>
2017-05-21 13:49:08 -0500

    export session

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,10 @@ console.log("Starting file");
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+console.log("Results ",notes.add(9,-2));
+/*var user = os.userInfo();
 console.log(user);
 fs.appendFile('Holo.txt',`Holo ${user.username} !`);
+*/
---
+++ notes-node/notes.js
@@ -0,0 +1,10 @@
+console.log("Starting notes.js");
+
+module.exports.addNote=()=>{
+  console.log('addNote Fucntion');
+  return 'Holo';
+};
+
+module.exports.add =(a,b)=>{
+  return (a+b);
+}
